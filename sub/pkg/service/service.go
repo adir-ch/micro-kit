04b6f39afb6fc53d7154f3b661eac12e0309f1da
adir-ch <actiger@gmail.com>
2018-10-05 18:08:57 +1000

    sub service

---
+++ sub/pkg/service/service.go
@@ -0,0 +1,30 @@
+package service
+
+import "context"
+
+// SubService describes the service.
+type SubService interface {
+	// Add your methods here
+	Sub(ctx context.Context, left, right float64) (rs float64, err error)
+}
+
+type basicSubService struct{}
+
+func (b *basicSubService) Sub(ctx context.Context, left float64, right float64) (rs float64, err error) {
+	// TODO implement the business logic of Sub
+	return rs, err
+}
+
+// NewBasicSubService returns a naive, stateless implementation of SubService.
+func NewBasicSubService() SubService {
+	return &basicSubService{}
+}
+
+// New returns a SubService with all of the expected middleware wired in.
+func New(middleware []Middleware) SubService {
+	var svc SubService = NewBasicSubService()
+	for _, m := range middleware {
+		svc = m(svc)
+	}
+	return svc
+}
